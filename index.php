e1ed2bffebabcfa746a06f601b638b295d0fc780
yaroslav <melnichuk.ya@gmail.com>
2016-01-06 18:19:13 +0200

    adding jquery

--- index.php
+++ index.php
@@ -85,7 +85,7 @@ if(isset($_POST['submit'])){
     </div>
 <?php }// конец елс?>
 
-<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script src="main.js"></script>
 </body>
 </html>
